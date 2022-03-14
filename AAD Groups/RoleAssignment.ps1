d07590f6ea080f7c857d8473698348b065f6c4d0
Matthew C <cantrell.matthew@live.com>
2022-03-14 18:52:46 -0500

    Fixed messaging in output array

--- AAD Groups/RoleAssignment.ps1
+++ AAD Groups/RoleAssignment.ps1
@@ -58,11 +58,11 @@ $MemberResults = foreach ($Group in $Groups) {
         [PSCustomObject]@{ #Export groups that do have members to array
             Group             = $Group.DisplayName
             "Group Object ID" = $Group.ObjectID
-            Owner             = "No members in AAD"
-            "Owner UPN"       = "No members in AAD"
-            "Owner Object ID" = "No members in AAD"
+            Member             = "No members in AAD"
+            "Member UPN"       = "No members in AAD"
+            "Member Object ID" = "No members in AAD"
         }
     }
 }
 $OwnerResults | export-csv ./GroupOwners.csv -NoTypeInformation
-$MemberResults | export-csv ./GroupOwners.csv -NoTypeInformation
+$MemberResults | export-csv ./GroupMembers.csv -NoTypeInformation
